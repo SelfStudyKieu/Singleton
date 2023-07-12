86cfca571c77396dd156d5c9ee6d9f2b76ca473a
kieuminhduc01 <kieuminhduc01@gmail.com>
2023-07-12 16:53:38 +0700

    first commit

---
+++ DesignPatternSingleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesignPatternSingleton;
+
+Thread t1 = new Thread(() => 
+{
+    var instance = UploadService.Instance(1);
+});
+
+Thread t2 = new Thread(() =>
+{
+    var instance = UploadService.Instance(2);
+});
+
+t1.Start();
+t2.Start();
---
+++ DesignPatternSingleton/UploadService.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatternSingleton
+{
+    class UploadService
+    {
+        private UploadService() { }
+
+        public int Id { get; private set; }
+
+        private static UploadService _instance;
+        private static readonly object _lock = new object();
+
+        public static UploadService Instance(int id)
+        {
+            if (_instance == null)
+            {
+
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new UploadService();
+                        _instance.Id = id;
+                    }
+                } 
+            }
+
+            return _instance;
+        }
+    }
+}
